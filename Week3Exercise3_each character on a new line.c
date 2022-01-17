9382188c90f257857831ed9888eb567aa4a50e30
Peiwen Chi <90221867+Peiwen0712@users.noreply.github.com>
2022-01-17 00:20:38 +0000

    Create Week3Exercise3_each character on a new line.c

---
+++ Week3Exercise3_each character on a new line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * Modify the function below so that it prints out each character in the string
+ * on a new line.  Hint: the end of a string is signified by the '\0' char
+ */
+
+
+void printCharsInString(char string[]){
+  int i = 0;
+  
+  while (string[i] != '\0'){
+    printf("%c\n", string[i]);
+    i++;
+  }
+  
+}
+
+int main(void) {
+  //printCharsInString("This is a string");
+  char my_string[] = "This is a string";
+  printCharsInString(my_string);
+  return 0;
+}
